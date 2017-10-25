b6ab881a37b3fe2e85c4deabccdffce55009c3dd
mohaila <245756054@qq.com>
2017-10-25 10:12:28 +0800

    创建gitignore

--- week1/day1/js/1.js
+++ week1/day1/js/1.js
@@ -37,12 +37,7 @@ var str = 'http://www.zhufengpeixun.com/stu/?name=zxt&age=28&sex=0#teacher';
 var result = queryURLParameter(str);
 //=>result的结果为：
 /*
- {
- name:'zxt',
- age:28,
- sex:0,
- HASH:'teacher'
- }
- */
 
+ */
+//如果我加一段话
 //https://www.baidu.com/s?wd=JavaScript&rsv_spt=1&rsv_iqid=0xb47fc54c00022c8c&issp=1&f=8&rsv_bp=0&rsv_idx=2&ie=utf-8&tn=98012088_5_dg&ch=12&rsv_enter=1&rsv_sug3=7&rsv_sug1=6&rsv_sug7=100&rsv_t=083brCA5MNTXEjXn4OsNUOF%2FrivBYMIADUrCBjMuRC7J%2ForhPlNVsV8SSJv7%2B76qQfE8iA&rsv_sug2=0&inputT=4129&rsv_sug4=4994
